a57350c6301a47f506beb778b5e8196f47f267f3
root <root@localhost.localdomain>
2021-08-03 18:53:06 +0300

    add vehicle class

--- assets/models.py
+++ assets/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
+
 # Create your models here.
+vehicle_weight = (
+    ("Heavy vehicle", "Heavy vehicle"),
+    ("Light vehicle", "Light vehicle"),
+)
+
+vehicle_brand = (
+    ("Toyota", "Toyota"),
+    ("Audi", "Audi"),
+    ("Nissan", "Nissan"),
+    ("Mercedes", "Mercedes"),
+    ("Vox Wagon", "Vox Wagon"),
+    ("BMW", "BMW"),
+    ("Range Rover", "Range Rover"),
+)
+
+computer_brand = (
+    ("Hp", "Hp"),
+    ("Del", "Del"),
+    ("Toshiba", "Toshiba")
+)
+
+computer_memory = (
+    ("500 Gb", "500 Gb"),
+    ("1 Tb", "1 Tb")
+)
+
+preprosser = (
+    ("Core i3", "Core i3"),
+    ("Core i5", "Core i5"),
+    ("Core i7", "Core i7"),
+)
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=30)
+    weight_class = models.CharField(max_length=30, choices=vehicle_weight)
+    vehicle_brand = models.CharField(max_length=30, choices=vehicle_brand)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    
